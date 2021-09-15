67175407c665ad67450b636efad4844980cc08c7
RootCellar <RootCellar9877@GMail.com>
2021-09-15 13:19:07 -0800

    begin work on ssarray class

---
+++ Project2/ssarray.h
@@ -0,0 +1,15 @@
+/*
+ * Darian Marvel
+ * 9/15/2021
+ * Header containing template class SSArray
+*/
+
+#ifndef INCLUDED_SSARRAY_H
+#define INCLUDED_SSARRAY_H
+
+template<typename Data_Type>
+class SSArray {
+
+};
+
+#endif
