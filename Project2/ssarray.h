29e801e15344bff5f522bbc79a6a44c8998efe75
RootCellar <RootCellar9877@GMail.com>
2021-09-15 13:29:35 -0800

    setup layout and data members

--- Project2/ssarray.h
+++ Project2/ssarray.h
@@ -7,9 +7,30 @@
 #ifndef INCLUDED_SSARRAY_H
 #define INCLUDED_SSARRAY_H
 
+//std::size_t
+#include <cstddef>
+
 template<typename Data_Type>
 class SSArray {
 
+//constructor, deconstructor, etc.
+public:
+
+//public functions
+public:
+
+//private functions
+private:
+
+//Public Data Members
+public:
+  using value_type = Data_Type;
+  using size_type = std::size_t;
+
+//Private Data Members
+private:
+  Data_Type* _arrPtr;
+  size_type _size;
 };
 
 #endif
