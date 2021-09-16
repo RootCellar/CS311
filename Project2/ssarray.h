6aa40e437f761b64b7b39ea68325ea4c7282ab6d
RootCellar <RootCellar9877@GMail.com>
2021-09-15 17:10:10 -0800

    implement some basic functions

--- Project2/ssarray.h
+++ Project2/ssarray.h
@@ -10,26 +10,81 @@
 //std::size_t
 #include <cstddef>
 
+//assert
+#include <cassert>
+
 template<typename Data_Type>
 class SSArray {
-
-//constructor, deconstructor, etc.
-public:
-
-//public functions
-public:
-
-//private functions
-private:
 
 //Public Data Members
 public:
   using value_type = Data_Type;
   using size_type = std::size_t;
 
+//constructors, deconstructor, etc.
+public:
+
+  SSArray(): _arrPtr(new value_type[8]), _size(8)
+  {}
+
+  explicit SSArray(size_type size)
+  {
+    assert(size >= 0);
+    _arrPtr = new value_type[size];
+    _size = size;
+  }
+
+  SSArray(size_type size, value_type item)
+  {
+    assert(size >= 0);
+    _arrPtr = new value_type[size];
+    _size = size;
+
+    for(size_type i = 0; i < _size; i++) {
+      _arrPtr[i] = item;
+    }
+  }
+
+  ~SSArray() {
+    delete [] _arrPtr;
+  }
+
+//public functions
+public:
+
+  value_type& operator[](size_type i) {
+    assert(i >= 0);
+    assert(i < _size);
+
+    return _arrPtr[i];
+  }
+
+  const value_type& operator[](size_type i) const {
+    assert(i >= 0);
+    assert(i < _size);
+
+    return _arrPtr[i];
+  }
+
+  value_type* begin() const {
+    return _arrPtr;
+  }
+
+  value_type* end() const {
+    return _arrPtr + _size;
+  }
+
+  size_type size() const {
+    return _size;
+  }
+
+//private functions
+private:
+
+
 //Private Data Members
 private:
-  Data_Type* _arrPtr;
+  value_type* _arrPtr;
   size_type _size;
 };
 
