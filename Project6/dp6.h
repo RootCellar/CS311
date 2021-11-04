37ed3ea8bb2b8b38f0eda502b6456f809e0f2af9
RootCellar <RootCellar9877@GMail.com>
2021-11-04 12:14:30 -0800

    finish project 6

--- Project6/dp6.h
+++ Project6/dp6.h
@@ -12,7 +12,9 @@
 
 #include "llnode2.h"
 
-//Exception Neutral
+//Reverses the ordering of an LLNode2 list,
+//given a unique_ptr to the head.
+//Does not throw
 template<typename ValType>
 void reverseList(std::unique_ptr<LLNode2<ValType>> & head) {
   //handle lists where size < 2
@@ -46,6 +48,12 @@ void reverseList(std::unique_ptr<LLNode2<ValType>> & head) {
 
 }
 
+// Requirements on Types:
+//  "vtype" or value_type must have a copy constructor
+//  "ktype" or key_type must also have a copy constructor
+//  both key_type and value_type must have a non-throwing destructor
+// Invariants:
+//  _data is either a nullptr or points to the valid head of the list
 template<typename ktype, typename vtype>
 class LLMap {
 
@@ -67,13 +75,10 @@ public:
 
   // Copy ctor
   LLMap(const LLMap<key_type, value_type> & other) = delete;
-
   // Move ctor
   LLMap(LLMap<key_type, value_type> && other) = delete;
-
   // Copy assignment operator
   LLMap<key_type, value_type> & operator=(const LLMap<key_type, value_type> & other) = delete;
-
   // Move assignment operator
   LLMap<key_type, value_type> & operator=(LLMap<key_type, value_type> && other) = delete;
 
@@ -96,11 +101,13 @@ public:
   }
 
   // No-Throw Guarantee
+  // Exception Neutral
   bool empty() const {
     return _data == nullptr;
   }
 
   // No-Throw Guarantee
+  // Exception Neutral
   const value_type* find(const key_type& key) const {
     if(_data == nullptr) return nullptr;
     auto p = _data.get();
@@ -112,6 +119,7 @@ public:
   }
 
   // No-Throw Guarantee
+  // Exception Neutral
   value_type* find(const key_type& key) {
     if(_data == nullptr) return nullptr;
     auto p = _data.get();
@@ -123,38 +131,52 @@ public:
   }
 
   // Strong Guarantee
+  // Exception Neutral
   void insert(const key_type& key, const value_type& value) {
     value_type* data = find(key);
 
+    //if we didn't find it, just put it at the front
     if(data == nullptr) push_front(_data, std::make_pair(key, value));
+    //otherwise, just change the data to the new value
     else *data = value;
   }
 
   // No-Throw Guarantee
+  // Exception Neutral
   void erase(const key_type& key) {
     auto p = _data.get();
     auto previous = p;
 
+    //find the node by key, keeping track of the previous node
     while(p != nullptr && p->_data.first != key) {
       previous = p;
       p = p->_next.get();
     }
 
+    //if a node with the given key does not exist, ...
     if(p == nullptr) return;
 
+    //if we are at the front of the list, ...
     if(previous == p) pop_front(_data);
     else {
+      //pretend previous->_next is the head of another list for a moment
+      //and pop it's head, also making previous->_next point to the new head
       pop_front(previous->_next);
     }
 
 
   }
 
+  // Pre:
+  //  func is a function that takes two parameters: key_type and value_type
   // Basic Guarantee
+  // Exception Neutral
   template<typename func>
   void traverse(func function) {
     auto p = _data.get();
 
+    //iterate through the list, calling the function on
+    //the key and value of each item.
     while(p != nullptr) {
       function(p->_data.first, p->_data.second); // may throw
       p = p->_next.get();
